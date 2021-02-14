d03838ad6c037a4d90e8eab96d1d6cc66fe2eb47
shm <39214521+shumpeipei@users.noreply.github.com>
2021-02-14 20:20:09 +0900

    修正しました

--- src/com/koike/janken.java
+++ src/com/koike/janken.java
@@ -1,10 +1,41 @@
 package com.koike;
-//test
-//test2
+
 import java.util.*;
 
 public class janken{
 
+
+    //引き分け判定
+    public static boolean  draw_judge(int num1, int num2){
+
+        boolean bool = true;
+
+        if(num1 == num2) {
+            bool = false;
+        }
+        return bool;
+    }
+
+
+    //判定処理
+    public  static String judge(int num1,int num2){
+        String win = null;
+
+        if(num1 == 0 && num2 == 1){
+            win = "あなた";
+        }else if(num1 ==1 && num2 ==2){
+            win = "あなた";
+        }else if (num1 ==2 && num2 ==0){
+            win = "あなた";
+        }else if(num2 == 0 && num1 == 1){
+            win = "コンピューター";
+        }else if(num2 ==1 && num1 ==2){
+            win = "コンピューター";
+        }else if (num2 ==2 && num1 ==0){
+            win = "コンピューター";
+        }
+        return win;
+    }
 
     public static void main(String[] args) {
 
@@ -13,52 +44,45 @@ public class janken{
 
 
         int cp_hands ;//cpのジャンケン
-        int player_hands ;//自分のジャンケン
+        int player_hands = 0;//自分のジャンケン
 
         String[] janken_hands = {"グー", "チョキ", "パー"};
         String winner =null;
         boolean bool = true;
 
 
-//プレイヤーのジャンケン入力
-//0-2以外の数値が入力されればエラーを吐く
+
+        //プレイヤーのジャンケン入力
+        //0-2以外の数値が入力されればエラーを吐く
         System.out.println("あなたは何を出す？(0:グー 1:チョキ 2:パー)");
 
+        try {
             player_hands = sc.nextInt();
+        }catch (Exception e){
+            System.out.println("文字が入力されています。0~2で入力してください。");
+            System.out.println("ゲームを終了します。");
+            System.exit(1);//プログラム強制終了
+        }
 
-            if(player_hands > 2){
-                System.out.println("0~2で入力してください。");
-                System.out.println("ゲームを終了します。");
-                System.exit(0);//プログラム終了
-            }
 
+        if(player_hands > 2){
+            System.out.println("0~2で入力してください。");
+            System.out.println("ゲームを終了します。");
+            System.exit(1);//プログラム強制終了
+        }
 
 
 
 //cpのジャンケンを乱数で生成
         cp_hands = rd.nextInt(2);
         System.out.println(cp_hands);
-//判定処理
-        if(player_hands == cp_hands){
-            bool = false;
-        }else if(player_hands == 0 && cp_hands == 1){
-            winner = "あなた";
-        }else if(player_hands ==1 && cp_hands ==2){
-            winner = "あなた";
-        }else if (player_hands ==2 && cp_hands ==0){
-            winner = "あなた";
-        }else if(cp_hands == 0 && player_hands == 1){
-            winner = "コンピューター";
-        }else if(cp_hands ==1 && player_hands ==2){
-            winner = "コンピューター";
-        }else if (cp_hands ==2 && player_hands ==0){
-            winner = "コンピューター";
-        }
 
 
+        bool = draw_judge(player_hands,cp_hands);//引き分け判定
 
+        winner = judge(player_hands,cp_hands);//勝敗判定
 
-//出力
+        //出力
         System.out.println("あなたが出した手は" + janken_hands[player_hands]);
         System.out.println("相手が出した手は" + janken_hands[cp_hands]);
 
@@ -67,7 +91,6 @@ public class janken{
         }else{
             System.out.println("今回は引き分けだっ");
         }
-
 
 
         }
