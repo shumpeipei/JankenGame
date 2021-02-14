3acf26e2a2a6ccb99d77c7c7eba0a857cabaa295
shm <39214521+shumpeipei@users.noreply.github.com>
2021-02-14 20:42:45 +0900

    修正しました

--- src/com/koike/janken.java
+++ src/com/koike/janken.java
@@ -5,6 +5,31 @@ import java.util.*;
 public class janken{
 
 
+    //プレイヤーのジャンケン入力
+    public static int input(){
+
+        Scanner sc = new Scanner(System.in);
+        int i=0;
+
+        //0-2以外の数値が入力されればエラーを吐く
+        System.out.println("あなたは何を出す？(0:グー 1:チョキ 2:パー)");
+
+        try {
+            i = sc.nextInt();
+        }catch (Exception e){
+            System.out.println("文字が入力されています。0~2で入力してください。");
+            System.out.println("ゲームを終了します。");
+            System.exit(1);//プログラム強制終了
+        }
+
+        if(i > 2){
+            System.out.println("0~2で入力してください。");
+            System.out.println("ゲームを終了します。");
+            System.exit(1);//プログラム強制終了
+        }
+        return i;
+    }
+
     //引き分け判定
     public static boolean  draw_judge(int num1, int num2){
 
@@ -15,6 +40,7 @@ public class janken{
         }
         return bool;
     }
+
 
 
     //判定処理
@@ -37,59 +63,39 @@ public class janken{
         return win;
     }
 
+    public static void output(int p_num, int c_num,boolean bool, String win){
+        String[] janken_hands = {"グー", "チョキ", "パー"};
+
+        System.out.println("あなたが出した手は" + janken_hands[p_num]);
+        System.out.println("相手が出した手は" + janken_hands[c_num]);
+
+        if(bool){
+            System.out.println("勝者は" + win + "だぁ！");
+        }else{
+            System.out.println("今回は引き分けだっ");
+        }
+        return;
+    }
+
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
 
-
-        int cp_hands ;//cpのジャンケン
+        int cp_hands = rd.nextInt(2); ;//cpのジャンケン
         int player_hands = 0;//自分のジャンケン
-
-        String[] janken_hands = {"グー", "チョキ", "パー"};
         String winner =null;
         boolean bool = true;
 
 
-
-        //プレイヤーのジャンケン入力
-        //0-2以外の数値が入力されればエラーを吐く
-        System.out.println("あなたは何を出す？(0:グー 1:チョキ 2:パー)");
-
-        try {
-            player_hands = sc.nextInt();
-        }catch (Exception e){
-            System.out.println("文字が入力されています。0~2で入力してください。");
-            System.out.println("ゲームを終了します。");
-            System.exit(1);//プログラム強制終了
-        }
-
-
-        if(player_hands > 2){
-            System.out.println("0~2で入力してください。");
-            System.out.println("ゲームを終了します。");
-            System.exit(1);//プログラム強制終了
-        }
-
-
-        //cpのジャンケンを乱数で生成
-        cp_hands = rd.nextInt(2);
-        System.out.println(cp_hands);
-
+        player_hands = input();//入力処理
 
         bool = draw_judge(player_hands,cp_hands);//引き分け判定
 
         winner = judge(player_hands,cp_hands);//勝敗判定
 
-        //出力
-        System.out.println("あなたが出した手は" + janken_hands[player_hands]);
-        System.out.println("相手が出した手は" + janken_hands[cp_hands]);
-
-        if(bool){
-            System.out.println("勝者は" + winner + "だぁ！");
-        }else{
-            System.out.println("今回は引き分けだっ");
-        }
+        output(player_hands,cp_hands,bool,winner);//判定出力
 
 
         }
